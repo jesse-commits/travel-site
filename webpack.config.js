f3b5cfea4d240ad4cb84212a5bd61bc81e7a9d96
Jesse <jessehglvn@gmail.com>
2021-08-12 22:30:09 -0400

    webpack server to display on smartphone using ip address.

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports ={
         },
         contentBase: path.join(__dirname, 'app'),
         hot: true,
-        port: 3000
+        port: 3000,
+        host: '0.0.0.0'
     },
     mode: 'development',
     module: {
